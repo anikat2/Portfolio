b5d4e14220e922f102fd61f83e583b8a730de907
anikat2 <anikathakur212@gmail.com>
2023-09-30 13:47:59 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToSkills() {
+    var skillsSection = document.getElementById('skills');
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToProjects() {
+    var skillsSection = document.getElementById('proj');
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+}
